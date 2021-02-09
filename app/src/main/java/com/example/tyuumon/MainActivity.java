c6903cc21b174a799da97919f013ac353c9d7d99
miaobaobao <joychoinjapan@gmail.com>
2021-02-09 21:57:58 +0900

    add tab menu

--- app/src/main/java/com/example/tyuumon/MainActivity.java
+++ app/src/main/java/com/example/tyuumon/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.tyuumon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.tyuumon.fragment.IndexFragment;
+import com.example.tyuumon.fragment.MeFragment;
+import com.example.tyuumon.fragment.ShoppingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.container,new IndexFragment());
+        fragmentTransaction.commit();
+    }
+
+    public void select_tab(View view){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (view.getId()){
+            case R.id.index:
+                fragmentTransaction.replace(R.id.container,new IndexFragment());
+                break;
+            case R.id.shopping:
+                fragmentTransaction.replace(R.id.container,new ShoppingFragment());
+                break;
+            case R.id.me:
+                fragmentTransaction.replace(R.id.container,new MeFragment());
+                break;
+            default:
+                fragmentTransaction.replace(R.id.container,new IndexFragment());
+                break;
+        }
+        fragmentTransaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/example/tyuumon/fragment/IndexFragment.java
@@ -0,0 +1,20 @@
+package com.example.tyuumon.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tyuumon.R;
+
+public class IndexFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_index, container, false);
+    }
+}
